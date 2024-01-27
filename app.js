43ea27e0c7be0d52c3cbf24539d74b6829c741dd
sh <sandrahammamitlili@gmail.com>
2024-01-27 18:00:00 +0100

    first-commit

---
+++ app.js
@@ -0,0 +1,19 @@
+const express=require('express');
+const app = express()
+
+//BodyParser Middleware
+app.use(express.json());
+
+// Appel de routes
+const categoriesRouter =require("./routes/categories.route")
+app.use('/api/categories', categoriesRouter);
+const scategoriesRouter =require("./routes/scategories.route")
+app.use('/api/scategories', scategoriesRouter);
+const articlesRouter =require("./routes/articles.route")
+app.use('/api/articles', articlesRouter);
+
+const PORT = process.env.PORT || 3001
+
+app.listen(PORT, () => console.log(`Server is running on port ${PORT} `))
+
+
